9f29ff0fd8dff93030acea79304e0f4ed34e957e
unknown <kwameagyenimboateng64@gmail.com>
2020-05-07 00:56:53 +0000

    animated button

--- App.js
+++ App.js
@@ -1,19 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TouchableWithoutFeedback,Animated, } from 'react-native';
 
-export default function App() {
+export default class Button extends React.Component {
+componentWillMount(){
+  this.animatedValue = new Animated.Value(1);
+}
+handlePressIn = ()=> {
+  Animated.spring(this.animatedValue,{
+    toValue:0.9
+  }).start()
+}
+handlePressOut = ()=> {
+  Animated.spring(this.animatedValue,{
+    toValue:1,
+    friction:20,
+    tension:30,
+  }).start()
+}
+
+ render(){
+   const animationStyle={
+     transform:[{scale:this.animatedValue}]
+   }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <View style={styles.buttonContainer}>
+        <TouchableWithoutFeedback   onPressIn={()=>{this.handlePressIn()}} onPressOut={()=>{this.handlePressOut()}} >
+            <Animated.View style={[styles.button,animationStyle]} >
+              <Text style={styles.text}>Sign up</Text>
+            </Animated.View>
+        </TouchableWithoutFeedback>
+     </View>
     </View>
   );
+ }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  buttonContainer:{
+    flex:1,
+    marginHorizontal:90,
+    justifyContent:'center',
+  },
+  button:{
+    padding:19,
+    borderRadius:5,
+    backgroundColor:"black",
+    elevation:5,
+  },
+  text:{
+    fontSize:16,
+    color:'white',
+    textAlign:'center'
+  }
 });
