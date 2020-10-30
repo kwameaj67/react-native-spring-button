95cfb405d33d6aad54111b376348dbe9e343f684
unknown <kwameagyenimboateng64@gmail.com>
2020-10-30 10:33:07 +0000

    change contents to func comp

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { StyleSheet,View,} from 'react-native';
 import Button from './Button'
-export default class Button extends React.Component {
- render(){
+const App = ()=> {
   return (
     <View style={styles.container}>
         <Button title ="Sign up"/>
     </View>
   );
- }
 }
-
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
