ca4a217fd390557fb03a7e6e1d65f5378e7e756e
unknown <kwameagyenimboateng64@gmail.com>
2020-10-30 10:30:35 +0000

    import button

--- App.js
+++ App.js
@@ -1,36 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View,TouchableWithoutFeedback,Animated, } from 'react-native';
-
+import { StyleSheet,View,} from 'react-native';
+import Button from './Button'
 export default class Button extends React.Component {
-componentWillMount(){
-  this.animatedValue = new Animated.Value(1);
-}
-handlePressIn = ()=> {
-  Animated.spring(this.animatedValue,{
-    toValue:0.9
-  }).start()
-}
-handlePressOut = ()=> {
-  Animated.spring(this.animatedValue,{
-    toValue:1,
-    friction:20,
-    tension:30,
-  }).start()
-}
-
  render(){
-   const animationStyle={
-     transform:[{scale:this.animatedValue}]
-   }
   return (
     <View style={styles.container}>
-     <View style={styles.buttonContainer}>
-        <TouchableWithoutFeedback   onPressIn={()=>{this.handlePressIn()}} onPressOut={()=>{this.handlePressOut()}} >
-            <Animated.View style={[styles.button,animationStyle]} >
-              <Text style={styles.text}>Sign up</Text>
-            </Animated.View>
-        </TouchableWithoutFeedback>
-     </View>
+        <Button title ="Sign up"/>
     </View>
   );
  }
@@ -41,20 +16,4 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  buttonContainer:{
-    flex:1,
-    marginHorizontal:90,
-    justifyContent:'center',
-  },
-  button:{
-    padding:19,
-    borderRadius:5,
-    backgroundColor:"black",
-    elevation:5,
-  },
-  text:{
-    fontSize:16,
-    color:'white',
-    textAlign:'center'
-  }
 });
